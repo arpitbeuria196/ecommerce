b6b248d58ec03e6359b9539c4b814f523da4f247
Arpit Beuria <arpitbeuria196@gmail.com>
2024-12-11 20:21:48 +0000

    productSchema

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -40,27 +40,26 @@ const userSchema = new mongoose.Schema({
     timestamps:true
 })
 
-const User = mongoose.model("User",userSchema);
 
-userSchema.pre("save",async function(next){
-    if(!this.isModified) return next();
 
-    try 
-    {
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    try {
         const salt = await bcrypt.genSalt(10);
-
-        this.password = await bcrypt.hash(this.password,salt);
+        this.password = await bcrypt.hash(this.password, salt);
         next();
-        
     } catch (error) {
         next(error);
     }
-})
+});
+
 userSchema.methods.comparePassword = async function (password) 
 {
     return await bcrypt.compare(password,this.password);
     
 }
 
+const User = mongoose.model("User",userSchema);
 
 export default User;
--- backend/redis/redis.js
+++ backend/redis/redis.js
@@ -1,3 +1,5 @@
 import Redis from "ioredis"
 
 export const redis = new Redis("rediss://default:AW5HAAIjcDE3YjAwMTc2NzllYTQ0MmI3OTc5NWUyN2MyNTgzNDVjYXAxMA@brief-hawk-28231.upstash.io:6379");
+
+
