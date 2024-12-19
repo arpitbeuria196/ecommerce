28c589ecf0383e473b97da148ac2e8ae86744423
Arpit Beuria <arpitbeuria196@gmail.com>
2024-12-19 23:06:44 +0000

    imgs

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -134,7 +134,7 @@ export const getProductsByCategory = async (req,res)=>
 
     try {
         const products = await Product.find({category});
-        res.json(products);
+        res.json({products});
     } catch (error) {
         res.status(500).json({message: "Server error",error: error.message});
     }
--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 //Parse the body of the request
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
 app.use(cors({
     origin:"http://localhost:5173",
